db98bb25a4192e4fa52bfcb0e2c26ac7e6f5d0d4
Ayu85 <ayushsinghdalimss@gmail.com>
2025-01-10 13:17:26 +0530

    update:login api integration

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
 import Navbar from './components/Navbar/Navbar'
 import { ThemeProvider } from './components/theme-provider'
+import { Toaster } from './components/ui/toaster'
 
 const App = () => {
   return (
-    <ThemeProvider defaultTheme='dark'  storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+      <Toaster />
       <Navbar />
       <Routes>
         <Route path='/' element={<Login />} />
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,20 +1,53 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Button } from '@/components/ui/button'
+import { axiosInstance } from '@/lib/axiosInstance'
+import { useToast } from '@/hooks/use-toast'
 const Login = () => {
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const { toast } = useToast()
+  const handleLogin = async () => {
+    try {
+      const response = await axiosInstance.post('/auth/login-user', {
+        email: emailRef.current.value,
+        password: passwordRef.current.value
+      })
+      if (response) {
+        toast({
+          title: response.data.message,
+          variant: 'success'
+        })
+      }
+    } catch (error) {
+      console.log(error);
+      
+      toast({
+        title: error.response.data.message || "Login failed",
+        variant: 'destructive'
+      })
+    }
+  }
   return (
     <div className='flex justify-center items-center pt-44'>
-      <div className='w-96 space-y-4 p-10 border border-zinc-600 rounded-md' >
+      <div className='w-96 space-y-4 p-10 border dark:border-zinc-800 border-zinc-200  rounded-md'>
         <div className='grid w-full max-w-sm items-center gap-1.5'>
           <Label htmlFor='email'>Email</Label>
-          <Input type='email' id='email' placeholder='Email' />
+          <Input ref={emailRef} type='email' id='email' placeholder='Email' />
         </div>
         <div className='grid w-full max-w-sm items-center gap-1.5'>
           <Label htmlFor='password'>Password</Label>
-          <Input type='password' id='password' placeholder='Password' />
+          <Input
+            ref={passwordRef}
+            type='password'
+            id='password'
+            placeholder='Password'
+          />
         </div>
-        <Button className='w-full'>Submit</Button>
+        <Button className='w-full' onClick={handleLogin}>
+          Submit
+        </Button>
       </div>
     </div>
   )
