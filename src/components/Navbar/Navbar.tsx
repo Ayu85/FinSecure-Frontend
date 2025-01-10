c545109475cb47a253790ddfae9aa5d23705ce02
Ayu85 <ayushsinghdalimss@gmail.com>
2025-01-10 11:36:32 +0530

    update:login page UI

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { ModeToggle } from '../mode-toogle'
+import { ShieldEllipsis } from 'lucide-react'
 
 const Navbar = () => {
   return (
-    <div>
-      <ModeToggle/>
+    <div className='p-4 flex justify-between dark:shadow-xl  dark:shadow-zinc-800 shadow-zinc-100 shadow-lg' >
+      <div className='flex items-center gap-1 text-teal-500'>
+        <h1 className='poppins-semibold'>FinSecure</h1>
+        <ShieldEllipsis />
+      </div>
+      <ModeToggle />
     </div>
   )
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Button } from '@/components/ui/button'
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='flex justify-center items-center pt-44'>
+      <div className='w-96 space-y-4 p-10 border border-zinc-600 rounded-md' >
+        <div className='grid w-full max-w-sm items-center gap-1.5'>
+          <Label htmlFor='email'>Email</Label>
+          <Input type='email' id='email' placeholder='Email' />
+        </div>
+        <div className='grid w-full max-w-sm items-center gap-1.5'>
+          <Label htmlFor='password'>Password</Label>
+          <Input type='password' id='password' placeholder='Password' />
+        </div>
+        <Button className='w-full'>Submit</Button>
+      </div>
+    </div>
   )
 }
 
